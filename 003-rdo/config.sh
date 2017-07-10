b58c781442fdc7f294a5faca9448634681428200
Harri Feng <harrifeng@gmail.com>
2017-07-10 16:38:24 +0800

    more control

--- 003-rdo/config.sh
+++ 003-rdo/config.sh
@@ -1,2 +1,13 @@
 # set the access mode for ssh config file
 chmod 600 ~/.ssh/config
+
+sudo systemctl disable firewalld
+sudo systemctl stop firewalld
+sudo systemctl disable NetworkManager
+sudo systemctl stop NetworkManager
+sudo systemctl enable network
+sudo systemctl start network
+
+sudo yum install -y centos-release-openstack-ocata
+sudo yum update -y
+sudo yum install -y openstack-packstack
