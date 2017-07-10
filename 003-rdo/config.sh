f1dbbbc6af5d640dd2a0ed6fde74c46f72edb3eb
Harri Feng <harrifeng@gmail.com>
2017-07-10 17:36:57 +0800

    restart then allinone

--- 003-rdo/config.sh
+++ 003-rdo/config.sh
@@ -1,6 +1,18 @@
 # set the access mode for ssh config file
 chmod 600 ~/.ssh/config
 
+sudo sed -i "s/ONBOOT=yes/ONBOOT=no/g" /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo "-------------------------------->"
+cat /etc/sysconfig/network-scripts/ifcfg-enp0s3
+sudo sed -i "s/PEERDNS=no/#PEERDNS=no/g" /etc/sysconfig/network-scripts/ifcfg-enp0s8
+echo "
+NETMASK=255.255.252.0
+GATEWAY=192.168.0.253
+DNS1=192.168.0.7
+" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-enp0s8
+echo "-------------------------------->"
+cat /etc/sysconfig/network-scripts/ifcfg-enp0s8
+
 sudo systemctl disable firewalld
 sudo systemctl stop firewalld
 sudo systemctl disable NetworkManager
