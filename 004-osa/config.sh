c48019755df96ad8850188fd8ab6bb67aa633af3
Harri Feng <harrifeng@gmail.com>
2017-07-10 20:36:13 +0800

    workable

--- 004-osa/config.sh
+++ 004-osa/config.sh
@@ -1,2 +1,8 @@
 sudo sed -i "s/us/cn/g" /etc/apt/sources.list
-sudo apt-get update -y
+# sudo apt-get update -y
+
+sed -i "/netmask/a dns-nameservers 192.168.0.7" /etc/network/interfaces
+sed -i "/netmask/a gateway 192.168.0.253" /etc/network/interfaces
+
+sudo route add default gw 192.168.0.253
+sudo route -n | awk '{ if ($8 =="enp0s3" && $2 != "0.0.0.0") print "route del default gw " $2; }'
