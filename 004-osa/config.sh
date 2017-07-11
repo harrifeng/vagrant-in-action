4afe6881dbe9f65e62b2a0350367d71991a6f429
Harri Feng <harrifeng@gmail.com>
2017-07-11 13:06:00 +0800

    ssh authorized had problem

--- 004-osa/config.sh
+++ 004-osa/config.sh
@@ -12,8 +12,9 @@ bonding
 sudo mkdir -p /root/.ssh
 sudo cp /home/vagrant/.ssh/authorized_keys /root/.ssh/authorized_keys
 
-sed -i "/netmask/a dns-nameservers 192.168.0.7" /etc/network/interfaces
-sed -i "/netmask/a gateway 192.168.0.253" /etc/network/interfaces
+# sed -i "/netmask/a dns-nameservers 192.168.0.7" /etc/network/interfaces
+# sed -i "/netmask/a gateway 192.168.0.253" /etc/network/interfaces
+sudo cp /home/vagrant/interfaces /etc/network/interfaces
 
 sudo route add default gw 192.168.0.253
 sudo route -n | awk '{ if ($8 =="enp0s3" && $2 != "0.0.0.0") print "route del default gw " $2; }'
