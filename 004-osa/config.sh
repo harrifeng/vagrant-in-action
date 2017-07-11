d38c1393ffe96f62c4de6aa6bdd076f08cf77dc8
Harri Feng <harrifeng@gmail.com>
2017-07-11 11:01:52 +0800

    folder creation

--- 004-osa/config.sh
+++ 004-osa/config.sh
@@ -2,13 +2,14 @@ sudo sed -i "s/us/cn/g" /etc/apt/sources.list
 sudo apt-get update -y
 
 # https://docs.openstack.org/project-deploy-guide/openstack-ansible/ocata/targethosts-prepare.html
-sudo apt-get install -y bridge-utils debootstrap ifenslave ifenslave-2.6 lsof lvm2 ntp ntpdate openssh-server sudo tcpdump vlan
+sudo apt-get install -y bridge-utils debootstrap ifenslave ifenslave-2.6 lsof lvm2 ntp ntpdate openssh-server sudo tcpdump vlan traceroute
 
 echo "
 bonding
 8021q " | sudo tee -a /etc/modules
 
 # use insecure vagrant keys
+sudo mkdir -p /root/.ssh
 sudo cp /home/vagrant/.ssh/authorized_keys /root/.ssh/authorized_keys
 
 sed -i "/netmask/a dns-nameservers 192.168.0.7" /etc/network/interfaces
