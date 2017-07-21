52ff3ae5416b169a7c2cad9792c867af7da77df6
Harri Feng <harrifeng@gmail.com>
2017-07-21 12:00:26 +0800

    need package

--- 007-rdo-ex/config.sh
+++ 007-rdo-ex/config.sh
@@ -5,9 +5,7 @@ sudo systemctl stop NetworkManager
 sudo systemctl enable network
 sudo systemctl start network
 
-sudo yum install -y centos-release-openstack-ocata
-
-sudo yum install -y openstack-packstack
+sudo yum install -y git tmux centos-release-openstack-ocata openstack-packstack
 
 sudo route add default gw 192.168.0.253
 sudo route del default gw 10.0.2.2
