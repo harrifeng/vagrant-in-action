bcbddeb9621ef21a215f8ebf5792dbcba3daa7fb
Harri Feng <harrifeng@gmail.com>
2017-07-21 12:01:47 +0800

    to install one by one

--- 007-rdo-ex/config.sh
+++ 007-rdo-ex/config.sh
@@ -5,7 +5,9 @@ sudo systemctl stop NetworkManager
 sudo systemctl enable network
 sudo systemctl start network
 
-sudo yum install -y git tmux centos-release-openstack-ocata openstack-packstack
+sudo yum install -y git tmux
+sudo yum install -y centos-release-openstack-ocata
+sudo yum install -y openstack-packstack
 
 sudo route add default gw 192.168.0.253
 sudo route del default gw 10.0.2.2
