7c8fc2fe9a56759307e7975775f6ea907b777b83
Haoran Feng <haoran.feng@alo7.com>
2017-08-10 14:01:01 +0800

    we may need to add demo-1 manually

--- 002-kolla/config-network-aio.sh
+++ 002-kolla/config-network-aio.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+
+# first to clean up all the container
+docker rm -vf `docker ps -a -q`
+/usr/share/kolla-ansible/tools/cleanup-host
+/usr/share/kolla-ansible/tools/cleanup-containers
+
 kolla-ansible deploy -i /root/inventory/all-in-one
 ip addr del 10.0.57.10/24 dev eth2
 ip link set eth2 up
