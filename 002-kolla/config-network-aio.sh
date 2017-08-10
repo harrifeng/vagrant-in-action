242e745cad3c7ecf217353b9915fc025a65af177
Haoran Feng <haoran.feng@alo7.com>
2017-08-10 13:21:29 +0800

    finished

---
+++ 002-kolla/config-network-aio.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+kolla-ansible deploy -i /root/inventory/all-in-one
+ip addr del 10.0.57.10/24 dev eth2
+ip link set eth2 up
+ip addr add 10.0.57.254/24 dev br-ex
+ip link set br-ex up
+kolla-ansible post-deploy
+source /etc/kolla/admin-openrc.sh
+/usr/share/kolla-ansible/init-runonce
