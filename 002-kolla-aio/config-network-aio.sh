76b52f1aabc0392ca5b2d850b4832fafc6ad55e6
Haoran Feng <haoran.feng@alo7.com>
2017-08-16 19:52:41 +0800

    create sdb

--- 002-kolla-aio/config-network-aio.sh
+++ 002-kolla-aio/config-network-aio.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
+
 # first to clean up all the container
 docker stop `docker ps -a -q`
 docker system prune --force
 /usr/share/kolla-ansible/tools/cleanup-host
 /usr/share/kolla-ansible/tools/cleanup-containers
+
+
+# we create the ceph disk
+parted /dev/sdb -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP 1 -1
 
 kolla-ansible deploy -i /root/inventory/all-in-one
 ip addr add 10.0.57.254/24 dev br-ex
